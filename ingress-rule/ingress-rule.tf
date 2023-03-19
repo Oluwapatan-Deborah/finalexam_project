6ec045f8b852af1ee74cc3c852839e8806d5182b
Oluwapatan Deborah <deboraholuwapatan@gmail.com>
2023-03-19 16:35:30 +0100

    first commit

---
+++ ingress-rule/ingress-rule.tf
@@ -0,0 +1,60 @@
+resource "kubernetes_ingress_v1" "voting-app" {
+  metadata {
+    name      = "azure-vote-front"
+    labels = {
+      name = "azure-vote-front"
+    }
+    annotations = {
+      "kubernetes.io/ingress.class" : "nginx"
+    }
+  }
+
+  spec {
+    rule {
+      host = "voting.dioluwapatan.me"
+      http {
+        path {
+          backend {
+            service{
+              name = "azure-vote-front"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "ingress" {
+  metadata {
+    name      = "sock-shop"
+    namespace = "sock-shop"
+    labels = {
+      name = "front-end"
+    }
+    annotations = {
+      "kubernetes.io/ingress.class" : "nginx"
+    }
+  }
+
+  spec {
+    rule {
+      host = "sock_shop_app.dioluwapatan.me"
+      http {
+        path {
+          backend {
+            service{
+              name = "front-end"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
